16238420e8020a8387051da3a6342836348a395e
Rushikesh Kumbhar <rushikeshkumbhar151@gmail.com>
2023-11-21 16:19:19 +0530

    all file

---
+++ Education_Service/Controllers/AdminCourseController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Education_Service.Controllers
+{
+    public class AdminCourseController : Controller
+    {
+      
+        // GET: AdminCourse
+        public ActionResult Index() //to show all courses
+        {
+            return View();
+        }
+        public ActionResult AddCourse()
+        { 
+            return View();
+        }
+        public ActionResult EditCourse()
+        {
+            return View();
+
+        } public ActionResult DeleteCourse() 
+        {
+            return View();
+        }
+    }
+}
